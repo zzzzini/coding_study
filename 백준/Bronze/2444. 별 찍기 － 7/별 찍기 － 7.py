cd938c28d868234c3a3f7f25b0fadee3752a0d46
zzzzini <hellguide02@naver.com>
2023-11-30 22:14:30 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 80 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,13 @@
+n = int(input())
+for i in range(1, n+1, 1):
+    for j in range(i,n,1):
+        print(" ", end='')
+    for j in range(0, 2*i-1, 1):
+        print("*", end='')
+    print()
+for i in range(n-1, 0, -1):
+    for j in range(i,n,1):
+        print(" ", end='')
+    for j in range(0, 2*i-1, 1):
+        print("*", end='')
+    print()
