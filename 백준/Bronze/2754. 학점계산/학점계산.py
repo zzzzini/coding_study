1dfda397ba03908ee2cd7f879746ab62cf238736
zzzzini <hellguide02@naver.com>
2023-11-30 22:32:17 +0900

    [Bronze V] Title: 학점계산, Time: 72 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,27 @@
+t = input()
+if t == "A+":
+    print(4.3)
+elif t == "A0":
+    print(4.0)
+elif t == "A-":
+    print(3.7)
+elif t == "B+":
+    print(3.3)
+elif t == "B0":
+    print(3.0)
+elif t == "B-":
+    print(2.7)
+elif t == "C+":
+    print(2.3)
+elif t == "C0":
+    print(2.0)
+elif t == "C-":
+    print(1.7)
+elif t == "D+":
+    print(1.3)
+elif t == "D0":
+    print(1.0)
+elif t == "D-":
+    print(0.7)
+elif t == "F":
+    print(0.0)
