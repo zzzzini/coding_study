905c49d1b9bd3df6319af131a01cf27a2180d772
zzzzini <hellguide02@naver.com>
2023-11-30 23:21:58 +0900

    [Bronze III] Title: 백발백준하는 명사수, Time: 72 ms, Memory: 32976 KB -BaekjoonHub

---
+++ 백준/Bronze/22938. 백발백준하는 명사수/백발백준하는 명사수.py
@@ -0,0 +1,8 @@
+import math
+x1, y1, r1 = map(int, input().split())
+x2, y2, r2 = map(int, input().split())
+d = math.sqrt((x1-x2) ** 2 + (y1-y2) ** 2)
+if math.sqrt((r1+r2) ** 2) > d:
+    print("YES")
+else:
+    print("NO")
