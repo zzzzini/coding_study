6723ada5fb9d94ea5871969f05a9ca11e6c9d3de
zzzzini <hellguide02@naver.com>
2023-11-30 21:57:46 +0900

    [Bronze III] Title: 꼬리를 무는 숫자 나열, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.py
@@ -0,0 +1,30 @@
+a, b = map(int, input().split())
+if a > b:
+    a, b = b, a
+lefta = a % 4
+leftb = b % 4
+
+if lefta == 0:
+    lefta += 4
+if leftb == 0:
+    leftb += 4
+
+if lefta > leftb:
+    big = lefta
+    small = leftb
+elif lefta == leftb:
+    big = lefta
+    small = leftb
+else:
+    big = leftb
+    small = lefta
+
+disc = big - small
+if lefta > disc:
+    newa = a - disc
+else:
+    newa = a + disc
+
+disl = (b - newa) // 4
+
+print(disc + disl)
