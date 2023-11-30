17ec8ac74ef5750e0ee6a3e1e7050bb716b2a099
zzzzini <hellguide02@naver.com>
2023-11-30 21:53:14 +0900

    [Bronze II] Title: 농구 경기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.py
@@ -0,0 +1,23 @@
+x = {}
+n = int(input())
+for i in range(n):
+    val = input()
+    temp = val[0]
+    if x.get(temp):
+        x[temp] = x[temp] + 1
+    else:
+        x[temp] = 1
+temp = []
+index = 0
+for key, val in x.items():
+    if int(val) >= 5:
+        temp.insert(index, key)
+        index += 1
+temp.sort()
+answer = ""
+for i in temp:
+    answer += i
+if answer == "":
+    print("PREDAJA")
+else:
+    print(answer)
