127ff8290d18eb85e1a5181f432cdf006e65d42a
zzzzini <hellguide02@naver.com>
2023-12-05 17:35:49 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,14 +1,9 @@
-import math
-
 n = int(input())
 l = input()
 hash = 0
-
-alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-key = [(i+1) for i in range(26)]
-
+mod = 1234567891
 count = 0
 for items in l:
-    hash += (key[alpha.index(items)] * math.pow(31, count))
+    hash += ((ord(items)-96) * (31 ** count))
     count += 1
-print("%d" %hash)
+print(hash % mod)
