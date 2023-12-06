2d3bdc00fb76a7c16409fa0216a9d3d7b727311f
zzzzini <hellguide02@naver.com>
2023-12-06 18:14:57 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+l = list()
+for i in range(n):
+    command = input().rstrip().split()
+    if command[0] == "push":
+        l.append(command[1])
+    elif command[0] == "pop":
+        if len(l) == 0:
+            print(-1)
+        else:
+            print(l.pop())
+    elif command[0] == "size":
+        print(len(l))
+    elif command[0] == "empty":
+        if len(l) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == "top":
+        if len(l) == 0:
+            print(-1)
+        else:
+            print(l[-1])
