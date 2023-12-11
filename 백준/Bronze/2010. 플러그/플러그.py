9e375003dd2b4fa4b1f3c23908f6edcc6af2c7b7
zzzzini <hellguide02@naver.com>
2023-12-11 09:02:12 +0900

    [Bronze III] Title: 플러그, Time: 192 ms, Memory: 35188 KB -BaekjoonHub

--- 백준/Bronze/2010. 플러그/플러그.py
+++ 백준/Bronze/2010. 플러그/플러그.py
@@ -1,8 +1,7 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-sum = 0
+array = []
 for i in range(0,n,1):
-    a = int(input())
-    sum += a-1
-print(sum+1)
+    array.append(int(input()))
+print(sum(array) - (n-1))
