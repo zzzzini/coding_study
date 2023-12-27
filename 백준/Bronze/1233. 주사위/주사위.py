a26de53305611d86ed2559d65458063b06582b0d
zzzzini <hellguide02@naver.com>
2023-12-27 09:11:29 +0900

    [Bronze II] Title: 주사위, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1233. 주사위/주사위.py
+++ 백준/Bronze/1233. 주사위/주사위.py
@@ -1,23 +1,15 @@
 a,b,c = map(int,input().split())
-s = []
-t = []
-for i in range(1,a+1,1):
-    for j in range(1,b+1,1):
-        for k in range(1,c+1,1):
-            s.append(i+j+k)
-            if i+j+k not in t:
-                t.append(i+j+k)
-s.sort()
-t.sort()
-count = [0] * len(t)
-for i in range(0,len(t),1):
-    for j in range(0,len(s),1):
-        if t[i] == s[j]:
-            count[i] += 1
-max = 0
-key = 0
-for i in range(0,len(count),1):
-    if count[i] > max:
-        max = count[i]
-        key = i
-print(t[key])
+
+dict = {}
+for i in range(a):
+    for j in range(b):
+        for k in range(c):
+            key = (i+1)+(j+1)+(k+1)
+            if dict.get(key):
+                dict[key] += 1
+            else:
+                dict[key] = 1
+
+answer = []
+[answer.append(k) for k, v in dict.items() if v == max(dict.values())]
+print(min(answer))
