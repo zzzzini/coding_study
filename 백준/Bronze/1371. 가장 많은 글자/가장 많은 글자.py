e0b742995a42ad15f00f5ea2c3f06d0c1953d1c6
zzzzini <hellguide02@naver.com>
2024-01-02 09:30:35 +0900

    [Bronze II] Title: 가장 많은 글자, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.py
+++ 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.py
@@ -6,10 +6,10 @@ for i in alp:
   af.append(text.count(i))
 mode = max(af)
 position = []
-for _ in range(af.count(mode)):
-  position.append(af.index(mode))
-  af[af.index(mode)] = -1
+
 ans = ""
-for s in position:
-  ans += alp[s]
+for _ in range(af.count(mode)):
+  ans += alp[af.index(mode)]
+  af[af.index(mode)] = -1
+
 print(ans)
