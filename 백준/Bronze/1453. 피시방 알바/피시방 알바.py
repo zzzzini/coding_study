7e0cf19b44f9ed3ffdba0e9a3767f7afaecaf6ac
zzzzini <hellguide02@naver.com>
2024-01-09 08:42:56 +0900

    [Bronze II] Title: 피시방 알바, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1453. 피시방 알바/피시방 알바.py
+++ 백준/Bronze/1453. 피시방 알바/피시방 알바.py
@@ -1,12 +1,4 @@
 n = int(input())
-num = []
-for i in range(1,101,1):
-    num.append(i)
 numbers = list(map(int,input().split()))
-counter = 0
-for i in range(0,n,1):
-    if numbers[i] in num:
-        num.remove(numbers[i])
-    else:
-        counter += 1
-print(counter)
+s_numbers = set(numbers)
+print(len(numbers) - len(s_numbers))
