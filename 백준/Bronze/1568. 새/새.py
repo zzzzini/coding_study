fc5aa299732fb2d8eac1904c5b16383519e46b4e
zzzzini <hellguide02@naver.com>
2024-01-09 16:19:33 +0900

    [Bronze II] Title: 새, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1568. 새/새.py
+++ 백준/Bronze/1568. 새/새.py
@@ -4,11 +4,11 @@ ad = 0
 while True:
     ad += 1
     t += 1
-    n -= ad
-    if n < 0:
-        n += ad
+    if n < ad:
         ad = 0
         t -= 1
+    else:
+        n -= ad
     if n == 0:
         break
 print(t)
