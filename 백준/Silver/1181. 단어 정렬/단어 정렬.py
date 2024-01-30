aabe19331965d4e287395039966c27799c5fc715
zzzzini <hellguide02@naver.com>
2024-01-30 09:47:49 +0900

    [Silver V] Title: 단어 정렬, Time: 76 ms, Memory: 33680 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,13 +1,12 @@
 import sys
 n = int(input())
-word = [['' for i in range(50)] for j in range(50)]
+word = [[] for i in range(50)]
 for i in range(n):
     s = sys.stdin.readline().strip()
     word[len(s)-1].append(s)
 for items in word:
-    temp = set(items)
-    items = list(temp)
-    items.sort()
-    for item in items:
-        if item != '':
+    if items != []:
+        temp = list(set(items))
+        temp.sort()
+        for item in temp:
             print(item)
