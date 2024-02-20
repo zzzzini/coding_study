19c3797a5e639bb6df6770469181eaed523c2567
zzzzini <hellguide02@naver.com>
2024-02-20 09:31:49 +0900

    [Silver V] Title: 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -0,0 +1,19 @@
+s = input()
+idx = 0
+zero_idx = [k for k, v in enumerate(s) if v == '0']
+one_idx = [k for k, v in enumerate(s) if v == '1']
+
+if len(zero_idx) == 0 or len(one_idx) == 0:
+    print(0)
+else:
+    cnt_zero = 1
+    cnt_one = 1
+    for i in range(1, len(zero_idx)):
+        if zero_idx[i] - zero_idx[i-1] != 1:
+            cnt_zero += 1
+
+    for i in range(1, len(one_idx)):
+        if one_idx[i] - one_idx[i-1] != 1:
+            cnt_one += 1
+
+    print(min(cnt_zero, cnt_one))
