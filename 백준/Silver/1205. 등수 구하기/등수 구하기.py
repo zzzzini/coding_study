b836147d8af512f20c5ea1bbc0a80d83c854d6f2
zzzzini <hellguide02@naver.com>
2024-03-12 09:59:33 +0900

    [Silver IV] Title: 등수 구하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.py
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -5,8 +5,7 @@ else:
     score = list(map(int, input().split()))
     score.append(t)
     score.sort(reverse=True)
-    if n == 0:
-        print(1)
+
     if score.index(t)+1 > p:
         print(-1)
     elif len([k for k in score if k>t]) + score.count(t) > p:
