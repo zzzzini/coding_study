e521edb447eadc29cb8b93905e107a4bee8fde53
zzzzini <hellguide02@naver.com>
2024-03-20 12:52:01 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,12 @@
+def solution(n):
+    ans = 0
+
+    while True:
+        if n == 0:
+            break
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n -= 1
+            ans += 1
+    return ans
